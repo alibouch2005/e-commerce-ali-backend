cd3cab3f88883be4eafb5702b020ce4b53c7a01b
Ali <ali@gmail.com>
2026-02-17 18:12:47 +0000

    Ajout de la route API de connexion avec middleware guest

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Récupère les informations d'identification de l'utilisateur à partir de la requête
+        $credentials = $request->only('email', 'password');
+
+        // Tente de connecter l'utilisateur avec les informations d'identification fournies
+        if (Auth::attempt($credentials)) { 
+            // Si la connexion est réussie, retourne les informations de l'utilisateur connecté
+            return response()->json(['user' => Auth::user()], 200);
+        }
+       // Si la connexion échoue, retourne une réponse d'erreur avec un message approprié
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\auth\LoginController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [LoginController::class, 'login'])->middleware('guest'); // Route pour la connexion des utilisateurs, accessible uniquement aux invités (non authentifiés)
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Routes protégées par l'authentification Sanctum, accessibles uniquement aux utilisateurs authentifiés
+    Route::get('/user', function (Request $request) {
+        return $request->user(); // Route pour récupérer les informations de l'utilisateur connecté
+    });
+});
 
