4217589b022f6ae9cd0ea578c5b0c0d701e15a7c
Ali <ali@gmail.com>
2026-02-17 16:54:58 +0000

    Ajouter un user ali sur seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Ali',
+            'email' => 'ali@gmail.com',
+            'password' => bcrypt('4122005'),
         ]);
     }
 }
