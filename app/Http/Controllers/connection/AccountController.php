6ff665a98a57384690bc5c034c7954460779287d
Ali <ali@gmail.com>
2026-02-24 17:19:32 +0000

    Edite sur fichier AccountController"

--- app/Http/Controllers/connection/AccountController.php
+++ app/Http/Controllers/connection/AccountController.php
@@ -11,26 +11,27 @@ use Illuminate\Support\Facades\Hash;
 class AccountController extends Controller
 {
     
-    function show(Request $request)
+   public function show(Request $request)
     {
-        return response()->json(['User' => $request->user()]);//return response()->json(['message' => 'AccountController show']);
+        $user = $request->user(); // Récupère l'utilisateur actuellement authentifié à partir de la requête
+        return response()->json(['User' => $user]);//return response()->json(['message' => 'AccountController show']);
 
     }
-    function update(UpdateRequest $request)
+   public function update(UpdateRequest $request)
     {
         $user = $request->user(); // Récupère l'utilisateur actuellement authentifié à partir de la requête
         $data =$request->validated(); // Valide les données de la requête en utilisant les règles définies dans UpdateRequest
         $user->update($data); // Met à jour les informations de l'utilisateur dans la base de données avec les données validées
         return response()->json(['message' => "Account update", 'user' => $user]);
     }
-    function delete(Request $request)
+  public function delete(Request $request)
     {
         $user = $request->user(); // Récupère l'utilisateur actuellement authentifié à partir de la requête
         auth()->guard('web')->logout(); // Déconnecte l'utilisateur en utilisant le guard 'web'
         $user->delete(); // Supprime l'utilisateur de la base de données
         return response()->json(['message' => "Account delete"]);
     }
-    function changePassword(ChangePasswordRequest $request)
+    public function changePassword(ChangePasswordRequest $request)
     {
         $user = $request->user(); // Récupère l'utilisateur actuellement authentifié à partir de la requête
         $user->update(['password'=>Hash::make($request->new_password)]);// Met à jour le mot de passe de l'utilisateur dans la base de données avec le nouveau mot de passe haché
