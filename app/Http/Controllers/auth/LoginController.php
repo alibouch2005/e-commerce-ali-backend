0b39ab5488e73fee84f2f7ebb01f467241ea7943
Ali <ali@gmail.com>
2026-03-12 12:11:06 +0000

    feat(LoginController):Ajoute session pour éviter les attaques de fixation de session

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -17,6 +17,7 @@ class LoginController extends Controller
         // Tente de connecter l'utilisateur avec les informations d'identification fournies
         if (Auth::attempt($credentials)) { 
             // Si la connexion est réussie, retourne les informations de l'utilisateur connecté
+             $request->session()->regenerate();// Regénère la session pour éviter les attaques de fixation de session
             return response()->json(['user' => Auth::user()], 200);
         }
        // Si la connexion échoue, retourne une réponse d'erreur avec un message approprié
