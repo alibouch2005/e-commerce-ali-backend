d5e40e5b49f8765e6552c26a6a8ec3d0c5cab87f
Ali <ali@gmail.com>
2026-03-19 00:08:35 +0000

    feat(users): ajout récupération utilisateurs par rôle (livreur)

---
+++ app/Http/Controllers/auth/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->has('role')) {
+            return User::where('role', $request->role)->get();
+        }
+
+        return User::all();
+    }
+}
